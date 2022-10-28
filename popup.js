25329ecfcf986011758e60e5fa4b5797192b4dc3
davidcrab <davidcrabtree18@icloud.com>
2022-10-28 16:22:20 -0400

    updated notion page properties

--- popup.js
+++ popup.js
@@ -1,58 +1,3 @@
-
-async function sendToNotion() {
-
-  console.log("starting function");
-
-  var myHeaders = new Headers();
-  myHeaders.append("Content-Type", "application/json");
-  myHeaders.append("Notion-Version", "2022-02-22");
-  myHeaders.append("Authorization", "Bearer secret_sTYLsPO0EfVNI0cI2Qf4EokpaE2RZ0wTbTFzYIjFcHm");
-
-  var raw = JSON.stringify({
-    "parent": {
-      "database_id": "5e46c626e0e941b5a1e52aceead7b06c"
-    },
-    "properties": {
-      "Name": {
-        "title": [
-          {
-            "text": {
-              "content": "Testing Fetch w Button"
-            }
-          }
-        ]
-      },
-      "URL": {
-        "type": "url",
-        "url": "https://jvns.ca/blog/2019/06/23/a-few-debugging-resources/"
-      },
-      "Tags": {
-        "type": "multi_select",
-        "multi_select": [
-          {
-            "id": "c91d5747-3447-4319-adca-9427c3744ef1",
-            "name": "read me",
-            "color": "orange"
-          }
-        ]
-      }
-    }
-  });
-
-  var requestOptions = {
-    method: 'POST',
-    headers: myHeaders,
-    body: raw,
-    redirect: 'follow'
-  };
-
-  const resp = await fetch("https://api.notion.com/v1/pages/", requestOptions)
-    .then(response => response.text())
-    .then(result => console.log(result))
-    .catch(error => console.log('error', error));
-  
-}
-
 async function sendToNotionTwo(url, title, description, image, icon) {
 
   console.log("starting function");
@@ -102,15 +47,13 @@ async function sendToNotionTwo(url, title, description, image, icon) {
         "type": "url",
         "url": url
       },
-      "Tags": {
-        "type": "multi_select",
-        "multi_select": [
-          {
+      "Status": {
+        "type": "status",
+        "status": {
             "id": "c91d5747-3447-4319-adca-9427c3744ef1",
             "name": "read me",
             "color": "orange"
-          }
-        ]
+        }
       }
     }
   });
@@ -173,8 +116,9 @@ async function markAsRead(id) {
   var raw = JSON.stringify({
     "properties": {
       "Status": {
-        "select": {
-          "name": "thanks for reading"
+        "type": "status",
+        "status": {
+            "name": "thanks for reading"
         }
       }
     }
